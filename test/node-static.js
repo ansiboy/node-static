83d7d9543b06b0dfbafea43460e030e2d6fb79d9
maishu <ansiboy@163.com>
2020-01-05 23:44:14 +0800

    增加单元测试

--- test/node-static.js
+++ test/node-static.js
@@ -37,6 +37,7 @@ describe("Server", function () {
         // 将文件夹 data/dir2 文件夹加入后，data/dir2/1.txt 将会覆盖掉 data/dir1/1.txt
         let root = new VirtualDirectory(path.join(__dirname, "data/dir1"));
         root.addPhysicalPath(path.join(__dirname, "data/dir2"));
+        root.addvirtualDirectory("data-dir3", path.join(__dirname, "data/dir3"), "merge");
         //=================================================================
 
         it("文件路径覆盖", async function () {
@@ -63,6 +64,7 @@ describe("Server", function () {
         it("新增文件读取", async function () {
             let server = new Server(root);
             let r = await server.servePath("3.txt");
+
             assert.notEqual(r.fileStream, null);
             assert.equal(r.statusCode, StatusCode.OK);
 
@@ -79,6 +81,27 @@ describe("Server", function () {
             })
 
             assert.equal(content, "dir2-3.txt");
+        })
+
+        it("虚拟路径文件读取", async function () {
+            let server = new Server(root);
+            let r = await server.servePath("data-dir3/1.txt");
+            assert.notEqual(r.fileStream, null);
+            assert.equal(r.statusCode, StatusCode.OK);
+
+            let content = await new Promise((resolve, reject) => {
+                let chunks = [];
+                r.fileStream.on("data", function (chunk) {
+                    chunks.push(chunk);
+                }).on("end", function () {
+                    let buffer = Buffer.concat(chunks);
+                    resolve(buffer.toString());
+                }).on("error", function (err) {
+                    reject(err);
+                })
+            })
+
+            assert.equal(content, "dir3-1.txt");
         })
 
     })
